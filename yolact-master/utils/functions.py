a353bd7dc746967d4f8e396f15103953fad1c12d
luzairun <54829406+luzairun@users.noreply.github.com>
2020-02-14 00:19:30 +0900

    Update functions.py

--- yolact-master/utils/functions.py
+++ yolact-master/utils/functions.py
@@ -117,37 +117,37 @@ class SavePath:
         file_name = os.path.basename(path)
         
         if file_name.endswith('.pth'):
-            file_name = file_name[:-4]
+            file_name = file_name[:-4] # 확장자명 제거
         
         params = file_name.split('_')
 
         if file_name.endswith('interrupt'):
             params = params[:-1]
         
-        model_name = '_'.join(params[:-2])
-        epoch = params[-2]
+        model_name = '_'.join(params[:-2])  # str array를 원소 사이에 _ 껴서 하나로 합침.
+        epoch = params[-2]                  # get_path 에서 file_name에 넣은 값 토대로 참조
         iteration = params[-1]
         
-        return SavePath(model_name, int(epoch), int(iteration))
+        return SavePath(model_name, int(epoch), int(iteration))  # file_name에 epoch와 iteration을 넣어서 저장
 
     @staticmethod
-    def remove_interrupt(save_folder):
-        for p in Path(save_folder).glob('*_interrupt.pth'):
+    def remove_interrupt(save_folder):  # _interrupt.pth 로 끝나는 파일 연결 해제
+        for p in Path(save_folder).glob('*_interrupt.pth'):  # 경로 상에 있는 파일들의 목록   xxx_interrupt.pth로 된 파일
             p.unlink()
     
     @staticmethod
-    def get_interrupt(save_folder):
+    def get_interrupt(save_folder):  # _interrupt.pth로 끝나는 파일 이름 get
         for p in Path(save_folder).glob('*_interrupt.pth'): 
             return str(p)
         return None
     
     @staticmethod
-    def get_latest(save_folder, config):
+    def get_latest(save_folder, config):   # 저장된 config 파일 중 가장 최근(itertation 제일 높은) 파일 가져옴.
         """ Note: config should be config.name. """
         max_iter = -1
         max_name = None
-
-        for p in Path(save_folder).glob(config + '_*'):
+       
+        for p in Path(save_folder).glob(config + '_*'):  
             path_name = str(p)
 
             try:
@@ -161,10 +161,10 @@ class SavePath:
 
         return max_name
 
-def make_net(in_channels, conf, include_last_relu=True):
+def make_net(in_channels, conf, include_last_relu=True):   # config 세팅 값 가져와서 network로 구성
     """
-    A helper function to take a config setting and turn it into a network.
-    Used by protonet and extrahead. Returns (network, out_channels)
+    A helper function to take a config setting and turn it into a network.  
+    Used by protonet and extrahead. Returns (network, out_channels)          
     """
     def make_layer(layer_cfg):
         nonlocal in_channels
@@ -177,23 +177,27 @@ def make_net(in_channels, conf, include_last_relu=True):
         #
         # You know it would have probably been simpler just to adopt a 'c' 'd' 'u' naming scheme.
         # Whatever, it's too late now.
-        if isinstance(layer_cfg[0], str):
+        
+        # layer 가능한 패턴이 4가지 있는데 코드가 너무 방대해서 각각 이름 붙이기에는 늦었다..
+        # 따라서 cfg 앞의 값들을 보고 어떤 종류의 layer인지 판단
+        
+        if isinstance(layer_cfg[0], str):   # 맨 앞 값이 string인지 확인
             layer_name = layer_cfg[0]
 
-            if layer_name == 'cat':
-                nets = [make_net(in_channels, x) for x in layer_cfg[1]]
-                layer = Concat([net[0] for net in nets], layer_cfg[2])
+            if layer_name == 'cat':        # 'cat'인 경우만 존재하므로 이 경우에는 예외처리가 따로 없음, subnets 합쳐서 처리
+                nets = [make_net(in_channels, x) for x in layer_cfg[1]]  # subnetwork들의 list에서 하나씩 뽑아서 network로 만들어준다
+                layer = Concat([net[0] for net in nets], layer_cfg[2])   # 
                 num_channels = sum([net[1] for net in nets])
         else:
             num_channels = layer_cfg[0]
             kernel_size = layer_cfg[1]
 
-            if kernel_size > 0:
+            if kernel_size > 0:  # conv
                 layer = nn.Conv2d(in_channels, num_channels, kernel_size, **layer_cfg[2])
             else:
-                if num_channels is None:
+                if num_channels is None: # bilinear interpolate
                     layer = InterpolateModule(scale_factor=-kernel_size, mode='bilinear', align_corners=False, **layer_cfg[2])
-                else:
+                else:  # deconv
                     layer = nn.ConvTranspose2d(in_channels, num_channels, -kernel_size, **layer_cfg[2])
         
         in_channels = num_channels if num_channels is not None else in_channels
@@ -204,11 +208,11 @@ def make_net(in_channels, conf, include_last_relu=True):
         # if num_channels is None:
         #     return [layer]
         # else:
-        return [layer, nn.ReLU(inplace=True)]
+        return [layer, nn.ReLU(inplace=True)]   # 일단 layer에는 ReLU 넣어놓고 아래에서 net 리턴할 때 제외
 
     # Use sum to concat together all the component layer lists
     net = sum([make_layer(x) for x in conf], [])
     if not include_last_relu:
-        net = net[:-1]
+        net = net[:-1] # 마지막 layer 제거(ReLU)
 
     return nn.Sequential(*(net)), in_channels
